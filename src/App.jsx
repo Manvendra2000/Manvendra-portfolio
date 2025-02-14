bd79722ba0a62f2d5ab54b2a26d3d756c67d1185
Manvendra2000 <mzealpro@gmail.com>
2025-02-14 08:07:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Marquee from "./components/Marquee";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Work from "./components/Work";
+
+export default function App() {
+  return (
+    <main className="font-light text-white antialised selection:bg-line-300 selection:text-black"><Navbar/>
+    <Hero/>
+    <Marquee/>
+    <Projects/>
+    <About/>
+    <Work/>
+    <Contact/></main>
+  )
+}
